9d88445af26e8239f2de43ab2514ad01b3007ee0
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2022-02-02 07:18:50 +0900

    src/SDDK/gvec.cpp

--- src/SDDK/gvec.cpp
+++ src/SDDK/gvec.cpp
@@ -468,7 +468,7 @@ int Gvec::index_by_gvec(vector3d<int> const& G__) const
        #2 (negative and positive)  ____________ 3   4  0  1  2 _________
        #3 (all positive)           _____________________  0  1  2  3 ___
 
-       Remember how FFT frequencies are stored: first positive frequences, then negative in the reverse order
+       Remember how FFT frequencies are stored: first positive frequencies, then negative in the reverse order
 
        subtract first z-coordinate in column from the current z-coordinate of G-vector: in case #1 or #3 this
        already gives a proper offset, in case #2 storage of FFT frequencies must be taken into account
